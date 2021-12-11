1cefe5848ec064f356695145877e1a60dc24ca1c
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2021-12-11 17:15:48 +0100

    update

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios"
-axios.defaults.baseURL = "http://localhost:5000/";
+// axios.defaults.baseURL = "http://localhost:5000/";
 // axios.defaults.headers.common['Authorization']  =  "Bearer " + localStorage.getItem("token")
-// axios.defaults.baseURL = "https://api.opeolluwa..mdbgo.io/";
+axios.defaults.baseURL = "https://opeolluwa-proxies.mdbgo.io/";
